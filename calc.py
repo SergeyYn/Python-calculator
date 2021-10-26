ea0ca8ad44d1c5b92c20a77d36e52f5d2fc12450
SergeyYn <31241884+SergeyYn@users.noreply.github.com>
2021-10-26 23:31:15 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,44 @@
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+
+    if operation == '+':
+        pass
+
+    elif operation == '-':
+        pass
+
+    elif operation == '*':
+        pass
+
+    elif operation == '/':
+        pass
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+    # Add again() function to calculate() function
+    again()
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+calculate()
